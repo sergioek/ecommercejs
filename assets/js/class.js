48ca816646647726a5f32f0675972ba3984e59ae
Sergio Ezequiel Khairallah <khairallahsergio4@gmail.com>
2022-10-19 20:06:22 -0300

    second commit

---
+++ assets/js/class.js
@@ -0,0 +1,28 @@
+let arrayUsers=[ ];
+class User{
+    constructor(user,name,email,password){
+        this.user = user;
+        this.name = name;
+        this.email = email;
+        this.password = password; 
+        
+    }
+
+    newUserPerson(){
+        
+        let nameUser= this.user;
+        nameUser= {
+            user: this.user,
+            name: this.name,
+            email:this.email,
+            password:this.password,
+        }
+        arrayUsers.push(nameUser);
+        return arrayUsers;
+            
+    }
+    
+}
+
+const person = new User('sergioek','Sergio','khairallahsergio4@gmail.com','1234');
+person.newUserPerson();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,65 @@
-let arrayUsers=[ ];
-class User{
-    constructor(user,name,email,password){
-        this.user = user;
-        this.name = name;
-        this.email = email;
-        this.password = password; 
-        
+const newUser = ()=>{
+    let name= prompt('Ingrese su nombre:');
+    let user= prompt('Ingrese un usuario:');
+    let email= prompt('Ingrese un email:');
+    let password= prompt('Ingrese un password:');
+    
+    while(name ==='' || user ==='' || email ==='' || password ===''){
+        name= prompt('El campo nombre no puede estar vacio:');
+        user= prompt('El campo  usuario no puede estar vacio:');
+        email= prompt('El campo  email no puede estar vacio:');
+        password= prompt('El campo  password no puede estar vacio:');
     }
+   
+    
+   let userPerson = new User(name,user,email,password);
+   userPerson.newUserPerson();
+   alert('Se registró un nuevo usuario');
+}
 
-    newUserPerson(){
-        
-        let nameUser= this.user;
-        nameUser= {
-            user: this.user,
-            name: this.name,
-            email:this.email,
-            password:this.password,
-        }
-        arrayUsers.push(nameUser);
-        return arrayUsers;
-            
+let loginState;
+
+const logOut = ()=>{
+    if(loginState==true){
+        loginState=null;
+        alert('Sesión cerrada');
+    }else{
+        alert('Debe iniciar sesión');
+
     }
     
 }
 
+const login = ()=>{
+    if(loginState==null){
+        let loginEmail = prompt('Ingrese su correo');
+        let loginPassword = prompt('Ingrese su contraseña');
 
-const newUser = ()=>{
-   let name= prompt('Ingrese su nombre:');
-   let user= prompt('Ingrese un usuario:');
-   let email= prompt('Ingrese un email:');
-   let password= prompt('Ingrese un password:');
+        while(loginEmail==='' || loginPassword===''){
+            loginEmail = prompt('El campo email no puede estar vacio.Reingrese su email.');
+            loginPassword = prompt('El campo passoword no puede estar vacio.Reingrese su passoword.');
+        }
 
-   
-    while(name ==='' || user ==='' || email ==='' || password ===''){
-        alert('Datos obligatorios');
+        for (let i = 0; i < arrayUsers.length; i++) {
+            const user = arrayUsers[i];
 
-   }
-   
-   let userPerson = new User(name,user,email,password);
-   userPerson.newUserPerson();
-   console.log(arrayUsers)
+            if(user.email === loginEmail || user.password === loginPassword){
+                loginState=true;
+                alert('Se inicio sesion como' + ' ' + user.name);
+                break;
+    
+            }else{
+                  alert('El email o contraseña ingresados son icorrectos. Inicie sesión nuevamente.')
+            }
+        }
+
+        
+    }else{
+        alert('Ya existe un usuario logueado.')
+    }
+    
 }
+
 
 
 
